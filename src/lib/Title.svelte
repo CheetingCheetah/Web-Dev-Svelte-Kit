f3c78374e6619ed0c0f617472bcbf9370f83c966
Nathaniel Woodward <NJWoodward@outlook.com>
2025-11-17 10:25:19 -0500

    Added section tag

--- src/lib/Title.svelte
+++ src/lib/Title.svelte
@@ -2,8 +2,10 @@
     let {name} = $props()
 </script>
 
-<div class="outer bg-blue white">
-    <div class="inner">
-        <h1>{name}</h1>
+<section>
+    <div class="outer bg-blue white">
+        <div class="inner">
+            <h1>{name}</h1>
+        </div>
     </div>
-</div>
+</section>
