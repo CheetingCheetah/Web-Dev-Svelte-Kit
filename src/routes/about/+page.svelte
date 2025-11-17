0b9578f2146c176e13fb19418e1707c1cad8bd5a
Nathaniel Woodward <NJWoodward@outlook.com>
2025-11-17 10:25:36 -0500

    Started adding section

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,3 +3,15 @@
 </script>
 
 <Title name="About"/>
+
+<section>
+    <div class="outer">
+        <div class="inner">
+            <h2>Subheading 1</h2>
+
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum porro ducimus, nihil neque cum aperiam repellendus exercitationem. Nemo architecto laborum odio deleniti labore beatae cum ab sapiente aperiam excepturi, optio officia eligendi?</p>
+
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque pariatur eveniet vel saepe voluptate adipisci omnis nulla delectus amet provident, expedita molestias modi distinctio nobis temporibus facere, non explicabo laborum! Dolorum quo eum nobis suscipit? Animi, tempore. Quis aut eveniet ipsam doloremque?</p>
+        </div>
+    </div>
+</section>
