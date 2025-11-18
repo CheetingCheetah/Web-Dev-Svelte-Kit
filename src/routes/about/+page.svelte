4362d0b32b20501026f3b169edde0b4bdc6710ca
Nathaniel Woodward <NJWoodward@outlook.com>
2025-11-18 11:50:40 -0500

    Added photo and more text

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,12 +6,52 @@
 
 <section>
     <div class="outer">
-        <div class="inner">
-            <h2>Subheading 1</h2>
+        <div class="inner flow">
+            <h2>Subheading One</h2>
+            <div class="columns flow">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum porro ducimus, nihil neque cum aperiam repellendus exercitationem. Nemo architecto laborum odio deleniti labore beatae cum ab sapiente aperiam excepturi, optio officia eligendi?</p>
 
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum porro ducimus, nihil neque cum aperiam repellendus exercitationem. Nemo architecto laborum odio deleniti labore beatae cum ab sapiente aperiam excepturi, optio officia eligendi?</p>
-
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque pariatur eveniet vel saepe voluptate adipisci omnis nulla delectus amet provident, expedita molestias modi distinctio nobis temporibus facere, non explicabo laborum! Dolorum quo eum nobis suscipit? Animi, tempore. Quis aut eveniet ipsam doloremque?</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque pariatur eveniet vel saepe voluptate adipisci omnis nulla delectus amet provident, expedita molestias modi distinctio nobis temporibus facere, non explicabo laborum! Dolorum quo eum nobis suscipit? Animi, tempore. Quis aut eveniet ipsam doloremque?</p>
+                
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur cumque incidunt assumenda reiciendis non, doloribus eos officia inventore officiis quia sunt repellendus, quo mollitia ullam omnis architecto quis maiores ab vitae? Non fugit labore eius voluptatum. Sapiente fuga ullam dolorem delectus nesciunt eius, sint officia minima nobis quisquam? Vel laudantium ipsum commodi.</p>
+            </div>
         </div>
     </div>
 </section>
+
+<section>
+    <div class="outer bg-blue white">
+        <div class="inner flow">
+            <h2>Subheading Two</h2>
+            <div class="columns flow">
+                <p class="white">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum porro ducimus, nihil neque cum aperiam repellendus exercitationem. Nemo architecto laborum odio deleniti labore beatae cum ab sapiente aperiam excepturi, optio officia eligendi?</p>
+
+                <p class="white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque pariatur eveniet vel saepe voluptate adipisci omnis nulla delectus amet provident, expedita molestias modi distinctio nobis temporibus facere, non explicabo laborum! Dolorum quo eum nobis suscipit? Animi, tempore. Quis aut eveniet ipsam doloremque?</p>
+                
+                <p class="white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur cumque incidunt assumenda reiciendis non, doloribus eos officia inventore officiis quia sunt repellendus, quo mollitia ullam omnis architecto quis maiores ab vitae? Non fugit labore eius voluptatum. Sapiente fuga ullam dolorem delectus nesciunt eius, sint officia minima nobis quisquam? Vel laudantium ipsum commodi.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="outer">
+        <div class="inner flow wide-text">
+            <h2>Subheading One</h2>
+                <img class="float-right" src="/Flower-Field.webp" alt="A field of flowers">
+
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum porro ducimus, nihil neque cum aperiam repellendus exercitationem. Nemo architecto laborum odio deleniti labore beatae cum ab sapiente aperiam excepturi, optio officia eligendi?</p>
+
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque pariatur eveniet vel saepe voluptate adipisci omnis nulla delectus amet provident, expedita molestias modi distinctio nobis temporibus facere, non explicabo laborum! Dolorum quo eum nobis suscipit? Animi, tempore. Quis aut eveniet ipsam doloremque?</p>
+                
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur cumque incidunt assumenda reiciendis non, doloribus eos officia inventore officiis quia sunt repellendus, quo mollitia ullam omnis architecto quis maiores ab vitae? Non fugit labore eius voluptatum. Sapiente fuga ullam dolorem delectus nesciunt eius, sint officia minima nobis quisquam? Vel laudantium ipsum commodi.</p>
+                
+        </div>
+    </div>
+</section>
+
+<style>
+    .wide-text p {
+        max-width: unset;
+    }
+</style>
