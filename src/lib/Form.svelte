e04c2ece3c867b39475eed5684d4af1f918f8e23
Nathaniel Woodward <NJWoodward@outlook.com>
2025-11-18 12:01:10 -0500

    Started adding contact page

---
+++ src/lib/Form.svelte
@@ -0,0 +1,36 @@
+<form>
+    <div class="flow">
+        <div class="form-item">
+            <label for="first">First Name</label>
+            <input 
+            type="text" 
+            id="first" name="first" 
+            placeholder="Enter first name...." 
+            required />
+        </div>
+        <div class="form-item">
+            <label for="last">Last Name</label>
+            <input 
+            type="text" 
+            id="last" name="last" 
+            placeholder="Enter last name...." 
+            required />
+        </div>
+        <div class="form-item">
+            <label for="email">Email</label>
+            <input 
+            type="email" 
+            id="email" name="email" 
+            placeholder="Enter email adrress...." 
+            required />
+        </div>
+        <div class="form-item">
+            <label for="first">First Name</label>
+            <input 
+            type="text" 
+            id="first" name="first" 
+            placeholder="Enter first name...." 
+            required />
+        </div>
+    </div>
+</form>
