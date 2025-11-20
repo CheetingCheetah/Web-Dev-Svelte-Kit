5c2689db642b32798b959aef32be559608324c32
Nathaniel Woodward <NJWoodward@outlook.com>
2025-11-20 12:29:01 -0500

    Added contact page

--- src/lib/Form.svelte
+++ src/lib/Form.svelte
@@ -1,5 +1,5 @@
 <form>
-    <div class="flow">
+    <div class="form-wrapper">
         <div class="form-item">
             <label for="first">First Name</label>
             <input 
@@ -25,12 +25,52 @@
             required />
         </div>
         <div class="form-item">
-            <label for="first">First Name</label>
+            <label for="tel">Phone (Optional)</label>
             <input 
             type="text" 
-            id="first" name="first" 
-            placeholder="Enter first name...." 
-            required />
+            id="tel" name="tel" 
+            placeholder="Enter phone number...." 
+            />
         </div>
+        <div class="form-item span-2">
+            <label for="message">Message</label>
+            <textarea name="message" id="message" rows=10></textarea>
+        </div>
+        <button type="submit" class="bg-blue white">Submit Message</button>
     </div>
 </form>
+
+<style>
+    .form-wrapper{
+        max-width: 48rem;
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        gap: 1.5rem;
+        align-items: start;
+    }
+
+    .form-item{
+        display: grid;
+    }
+
+    .span-2{
+        grid-column: span 2;
+    }
+
+    label{
+        font-weight: 600;
+    }
+
+    input, textarea{
+        padding: 1rem;
+        font: inherit;
+    }
+
+    button{
+        justify-self: start;
+    }
+
+    button:hover{
+        background-image: var(--gradient-pink);
+    }
+</style>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -7,7 +7,7 @@
 
 <section>
 	<div class="outer">
-		<div class="inner">
+		<div class="inner flow">
 			<h2> Send us a message!</h2>
 			<Form />
 		</div>
