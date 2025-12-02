69c1a6657cece96e285b7add9227ee4c9df77a48
Nathaniel Woodward <NJWoodward@outlook.com>
2025-12-02 10:57:39 -0500

    Added auto pages for posts

---
+++ src/routes/blog/[id]/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Title from "$lib/Title.svelte";
+
+	export let data
+</script>
+
+<Title name="{data.post.title}"/>
+
+<div class="outer">
+    <div class="inner">
+        <p>{data.post.body}</p>
+    </div>
+</div>
