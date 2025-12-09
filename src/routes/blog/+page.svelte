decc0e51eeaeeb7d62928d770a0c55b126df0185
Nathaniel Woodward <NJWoodward@outlook.com>
2025-12-09 12:37:16 -0500

    Added images to the posts

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -11,6 +11,7 @@
 		<div class="inner auto-fit">
 			{#each data.posts.posts as post}
 				<div class="flow">
+					<a href="/blog/{post.slug}"><img src="{post.featured_image}" alt="{post.title}"></a>
 					<h2><a href="/blog/{post.slug}">{@html post.title}</a></h2>
 					<p>{@html post.excerpt}</p>
 				</div>
@@ -18,3 +19,10 @@
 		</div>
 	</div>
 </section>
+
+<style>
+	img{
+		aspect-ratio: 1/1;
+		object-fit: cover;
+	}
+</style>
